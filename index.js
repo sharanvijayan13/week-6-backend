c329d22c729736195f4011f5968517c99a19c8f2
sharanvijayan13 <sharanvijayan05@gmail.com>
2025-10-22 17:37:50 +0530

    Deploy

--- index.js
+++ index.js
@@ -34,4 +34,5 @@ app.post("/api/posts", async (req, res) => {
   res.json(data[0]);
 });
 
-app.listen(5000, () => console.log("http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
